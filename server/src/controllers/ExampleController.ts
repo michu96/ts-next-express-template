8e1caff711eceb82b43a859fdd32ff86012dd2c8
Michal Trudzinski <mihu1701@gmail.com>
2021-09-11 18:35:25 +0200

    add DELETE example api endpoint

--- server/src/controllers/ExampleController.ts
+++ server/src/controllers/ExampleController.ts
@@ -23,6 +23,20 @@ const ExampleController = {
             return res.status(500).json({ error: 'Server error' })
         }
     },
+
+    remove: async (req: Request, res: Response): Promise<Response> => {
+        try {
+            const example = await Example.findOne(req.params.id)
+            if (example) {
+                example.remove()
+                return res.sendStatus(204)
+            }
+            return res.sendStatus(404)
+        } catch (err) {
+            console.log(err)
+            return res.sendStatus(500)
+        }
+    },
 }
 
 export default ExampleController
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,5 +6,6 @@ const router = Router()
 
 router.get('/examples', ExampleController.getAll)
 router.post('/examples', ExampleMiddleware.verify, ExampleController.create)
+router.delete('/examples/:id', ExampleController.remove)
 
 export default router
